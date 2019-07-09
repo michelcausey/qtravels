8974c8eb69e976d9b547f97d081b80280279d9b7
MiChel Causey <michel.causey@gmail.com>
2019-07-09 18:25:36 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("started");
+
+// document.getElementById("item-1").addEventListener("click", function() {
+//   console.log("clicked item-1");
+// });
+
+// document.getElementById("item-2").addEventListener("click", function() {
+//   console.log("clicked item-2");
+// });
+
+// document.getElementById("item-3").addEventListener("click", function() {
+//   console.log("clicked item-3");
+// });
